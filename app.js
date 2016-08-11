9fbccae9bbca87b16fd31f2fa4d4bcdbdc8fa34c
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-08-11 17:02:20 +0300

    Somewhat working ui router

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
+'use strict';
 angular.module('leagueApp',["ui.router"])
-.config(['$urlRouterProvider', '$StateProvider',function($urlRouterProvider, $stateProvider){
+.config(['$urlRouterProvider', '$stateProvider',function($urlRouterProvider, $stateProvider){
     $stateProvider
         .state('home',{
             url: 'home',
-            template: 'home/home.html',
+            templateUrl: 'home/home.html',
             controller:'home/home.js'
         })
         .state('build.create',{
-            url: '#!/buildcreation',
-            template: 'build.create.html',
+            url: 'buildcreation',
+            templateUrl: 'build.create/build.create.html',
             controller:'build.create/build.create.js'
         })
         .state('build.search',{
             url: '/buildsearch',
-            template: 'build.search/build.search.html',
+            templateUrl: 'build.search/build.search.html',
             controller:'build.search/build.search.js'
         })
         .state('profile.edit',{
             url: '/profiledit',
-            template: 'profile.edit/profile.edit.html',
+            templateUrl: 'profile.edit/profile.edit.html',
             controller:'profile.edit/profile.edit.js'
         })
         .state('profile.search',{
             url: '/profilesearch',
-            template: 'profile.search/profile.search.html',
+            templateUrl: 'profile.search/profile.search.html',
             controller:'profile.search/profile.search.js'
         })
         .state('profile.view',{
             url: 'profileview',
-            template: 'profile.view/profile.view.html',
+            templateUrl: 'profile.view/profile.view.html',
             controller:'profile.view/profile.view.js'
         })
         .state('signup',{
             url: 'signup',
-            template: 'signup/signup.html',
+            templateUrl: 'signup/signup.html',
             controller:'signup/signup.js'
         })
 
