0810680963e2c602fd21d5a58f37da2ffef7e01a
RicardasFaturovas <ricardas.faturovas@gmail.com>
2016-08-17 15:36:42 +0300

    Better working dialog

--- build.create/build.create.js
+++ build.create/build.create.js
@@ -1,5 +1,6 @@
 'use strict';
-angular.module('leagueApp.buildCreate',[]).controller('buildCreate', ['$scope', 'items',function ($scope, items) {
+angular.module('leagueApp.buildCreate',[]).controller('buildCreate', ['$scope','items', 'itemDataTransferService', function ($scope, items, itemDataTransferService) {
     $scope.abc = items;
+    itemDataTransferService.itemDataSrv= items;
     console.log($scope.abc);
 }]);
--- components/directives/build.display.drv.js
+++ components/directives/build.display.drv.js
@@ -6,12 +6,13 @@ angular.module('leagueApp').directive('buildDisplayDirective', function () {
 
         templateUrl: 'components/directives/templates/build.display.tmpl.html',
 
-        controller: function($scope, $mdDialog, $mdMedia) {
+        controller: function($scope, $mdDialog, $mdMedia, itemDataTransferService) {
             $scope.status = '  ';
+            $scope.items = itemDataTransferService.itemDataSrv;
             $scope.showAdvanced = function (ev) {
                 var useFullScreen = ($mdMedia('sm') || $mdMedia('xs')) && $scope.customFullscreen;
                 $mdDialog.show({
-                        controller: function ($scope, $mdDialog,items ) {
+                        controller: function ($scope, $mdDialog) {
                             $scope.hide = function() {
                                 $mdDialog.hide();
                             };
@@ -21,18 +22,18 @@ angular.module('leagueApp').directive('buildDisplayDirective', function () {
                             $scope.answer = function(answer) {
                                 $mdDialog.hide(answer);
                             };
-                            $scope.items = items
+                            $scope.items = itemDataTransferService.itemDataSrv;
+
                         },
                         templateUrl: 'components/dialogs/templates/build.display.dialog.tmpl.html',
                         parent: angular.element(document.body),
-                    resolve: {
-                        items : function(itemGetService){
-                            return itemGetService.getItems();
-                        }
-                    },
+                  
                         targetEvent: ev,
                         clickOutsideToClose: true,
-                        fullscreen: useFullScreen
+                        fullscreen: useFullScreen,
+                    locals: {
+                        items: $scope.items
+                    }
                     })
                     .then(function (answer) {
                         $scope.status = 'You said the information was "' + answer + '".';
